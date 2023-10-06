907f93cc63a5deb3bb06868dec80de1319d424f1
Slobodan Jevtic <js170758d@student.etf.bg.ac.rs>
2023-10-06 12:31:07 +0200

    Add api and config variables

--- Makefile
+++ Makefile
@@ -19,4 +19,10 @@ sqlc:
 test:
 	go test -v -cover ./...
 
-.PHONY: postgres createdb dropdb migrateup migratedown sqlc test
+server:
+	go run main.go
+
+clean:
+	go clean -modcache
+
+.PHONY: postgres createdb dropdb migrateup migratedown sqlc test server clean
---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+
+	"github.com/slobodanjevtic/simple_bank/util"
+
+	_"github.com/lib/pq"
+	"github.com/slobodanjevtic/simple_bank/api"
+	db "github.com/slobodanjevtic/simple_bank/database/sqlc"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
+
+	
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("Cannot connect to database: ", err)
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(config.ServerAddress)
+	if err != nil {
+		log.Fatal("Cannot start server: ", err)
+	}
+}
